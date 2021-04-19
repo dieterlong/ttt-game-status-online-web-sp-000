6a64c4b05ea6ebd9dc7bc62da16f7855e85d1160
dieter long <dieterwaitforitlong@gmail.com>
2021-04-19 03:41:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,21 @@ WIN_COMBINATIONS =
   [1,4,7],
   [2,5,8],
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+  index_0 = win_combo[0]
+  index_1= win_combo[1]
+  index_2=win_combo[2]
+  position_1 = board[index_0]
+  position_2=board[index_1]
+  position_3= board[index_2]
+
+  if position_1 == "X" && position_2 =="X" && position_3 == "X"
+    win_combo
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    win_combo
+  end
+}
+false
+end
